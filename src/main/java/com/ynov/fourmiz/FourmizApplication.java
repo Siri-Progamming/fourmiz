a87057f60d7951ea22ec57dd8380c42630f9abbb
Sirika HILAIRE <sirika.hilaire@cgi.com>
2023-11-29 16:08:57 +0100

    init

---
+++ src/main/java/com/ynov/fourmiz/FourmizApplication.java
@@ -0,0 +1,13 @@
+package com.ynov.fourmiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourmizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourmizApplication.class, args);
+    }
+
+}
